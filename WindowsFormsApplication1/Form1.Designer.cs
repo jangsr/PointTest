e6fd22fe72aec3db0ed150f27b251e7492e16f53
장새롬 <jang-sr@hotmail.com>
2016-04-01 19:57:42 +0900

    db 까지

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -33,32 +33,36 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.tbFileUrl = new System.Windows.Forms.TextBox();
-            this.tbLog = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.tbCul = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbSql = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 113);
+            this.button1.Location = new System.Drawing.Point(9, 152);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(513, 56);
+            this.button1.Size = new System.Drawing.Size(513, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "실행";
+            this.button1.Text = "포인트 생성 실행";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tbNet
             // 
-            this.tbNet.Location = new System.Drawing.Point(85, 17);
+            this.tbNet.Location = new System.Drawing.Point(105, 17);
             this.tbNet.Name = "tbNet";
-            this.tbNet.Size = new System.Drawing.Size(440, 21);
+            this.tbNet.Size = new System.Drawing.Size(416, 21);
             this.tbNet.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Location = new System.Drawing.Point(22, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 2;
@@ -67,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 51);
+            this.label2.Location = new System.Drawing.Point(22, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 12);
             this.label2.TabIndex = 3;
@@ -75,23 +79,15 @@
             // 
             // tbFileUrl
             // 
-            this.tbFileUrl.Location = new System.Drawing.Point(85, 48);
+            this.tbFileUrl.Location = new System.Drawing.Point(105, 48);
             this.tbFileUrl.Name = "tbFileUrl";
-            this.tbFileUrl.Size = new System.Drawing.Size(440, 21);
+            this.tbFileUrl.Size = new System.Drawing.Size(416, 21);
             this.tbFileUrl.TabIndex = 4;
-            // 
-            // tbLog
-            // 
-            this.tbLog.Location = new System.Drawing.Point(13, 181);
-            this.tbLog.Multiline = true;
-            this.tbLog.Name = "tbLog";
-            this.tbLog.Size = new System.Drawing.Size(512, 320);
-            this.tbLog.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 87);
+            this.label3.Location = new System.Drawing.Point(22, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 12);
             this.label3.TabIndex = 3;
@@ -99,26 +95,64 @@
             // 
             // tbCul
             // 
-            this.tbCul.Location = new System.Drawing.Point(109, 83);
+            this.tbCul.Location = new System.Drawing.Point(105, 83);
             this.tbCul.Name = "tbCul";
             this.tbCul.Size = new System.Drawing.Size(417, 21);
             this.tbCul.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DB접속정보";
+            // 
+            // tbSql
+            // 
+            this.tbSql.Location = new System.Drawing.Point(105, 115);
+            this.tbSql.Name = "tbSql";
+            this.tbSql.Size = new System.Drawing.Size(417, 21);
+            this.tbSql.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(513, 42);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "DB로 넣기 실행";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 513);
-            this.Controls.Add(this.tbLog);
+            this.ClientSize = new System.Drawing.Size(537, 264);
+            this.Controls.Add(this.tbSql);
             this.Controls.Add(this.tbCul);
             this.Controls.Add(this.tbFileUrl);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbNet);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Test";
+            this.Text = "포인트 생성";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,9 +165,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbFileUrl;
-        private System.Windows.Forms.TextBox tbLog;
         private System.Windows.Forms.TextBox tbCul;
         private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbSql;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -14,18 +15,47 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Queue<string> que = new Queue<string>();
+        Queue<string> queSql = new Queue<string>();
+        private BackgroundWorker worker;
+        private BackgroundWorker worker2;
+
         public Form1()
         {
             InitializeComponent();
             tbFileUrl.Text = @"C:\Users\JSR\Desktop\인터페이스_1\009. 전력량_MODBUS(node91~98)\MODBUS";
             tbNet.Text = "1.1";
             tbCul.Text = "기동시간적산,가스량계,E_Cooling,E_Heating,KWH,적산,누적";
+            tbSql.Text = "Data Source=localhost;Initial Catalog=" + "OEMSMST" + ";USER ID=" + "sa" + ";PASSWORD=" + "pass@word!02" + ";Connection Timeout=3600";
+
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+
+            worker2 = new BackgroundWorker();
+            worker2.WorkerReportsProgress = true;
+            worker2.WorkerSupportsCancellation = true;
+            worker2.DoWork += new DoWorkEventHandler(backgroundWorker2_DoWork);
+            worker2.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker2_RunWorkerCompleted);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            worker.RunWorkerAsync();
+            Run();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            worker2.RunWorkerAsync();
+        }
+
+        private void Run()
+        {
             var filePath = tbFileUrl.Text;
-            var outputFileName = tbFileUrl.Text.Contains("MODBUS") ? "MODBUS" : "BACNET";
 
             if (tbNet.Text == string.Empty)
             {
@@ -59,19 +89,25 @@ namespace WindowsFormsApplication1
                         //OBJECT_NAME	DEVICE_INST	OBJECT_TYPE	OBJECT_INST
                         for (int j = 0; j < result.Tables[i].Rows.Count; j++)
                         {
-                            var pointCul = "순시값";
+                            var pointCul = "순시값"; 
                             var pointName = result.Tables[i].Rows[j]["OBJECT_NAME"].ToString();
                             var pointAddr = string.Format("{0}.{1}.{2}-{3}", tbNet.Text, result.Tables[i].Rows[j]["DEVICE_INST"].ToString(), result.Tables[i].Rows[j]["OBJECT_TYPE"].ToString(), result.Tables[i].Rows[j]["OBJECT_INST"].ToString());
-                            resultData = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16}", pointAddr, pointName.Replace(",",""),string.Empty, string.Empty, "Real", "사용중", "사용중",string.Empty,string.Empty,string.Empty, string.Empty, string.Empty, "Analog", "R", CheckCul(pointName), string.Empty, pointName.Replace(",", ""));
-                            Log(resultData, outputFileName);
+                            var cul = CheckCul(pointName);
+                            resultData = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16}", pointAddr, pointName.Replace(",",""),string.Empty, string.Empty, "Real", "사용중", "사용중",string.Empty,string.Empty,string.Empty, string.Empty, string.Empty, "Analog", "R", cul, string.Empty, pointName.Replace(",", ""));
+                            var tbPoint = "INSERT INTO tbPoint VALUES ( " + string.Format(" '{0}', N'{1}', {2}, '{3}', '{4}', {5}, {6}, {7}, GETDATE(), 'setup', GETDATE(), 'setup')", pointAddr, pointName.Replace("'", ""), 9999, string.Empty, "R", 1, 1, 9999);
+                            var tbPointProp = "DECLARE @Seq int; select @Seq = PointSeq from tbPoint where pt_addr = '" + pointAddr + "'; INSERT INTO tbPointProp VALUES ( " + string.Format("@Seq, '{0}', '{1}', {2}, {3}, '{4}', '{5}', '{6}', N'{7}',{8})", string.Empty, string.Empty, 0, 0, 'A', string.Empty, cul == "누적값" ? "C" : "I", pointName.Replace("'", ""), -1);
+                            que.Enqueue(resultData);
+                            queSql.Enqueue(tbPoint);
+                            queSql.Enqueue(tbPointProp);
                         }
                     }
                 }
 
                 excelReader.Close();
             }
-
+            queSql.Enqueue("insert into FMS_Master.dbo.tbTotal select PointSeq as pt_index, pt_addr, pt_name, 0 as pt_value, 0 as LogTime from vwPoint where pt_addr not in (select pt_addr from FMS_Master.dbo.tbTotal)");
         }
+
 
         public string CheckCul(string _str)
         {
@@ -101,8 +137,8 @@ namespace WindowsFormsApplication1
             // 삭제 기준 Log 날짜
             var dt = DateTime.Today.AddDays(-7);
 
-            string FilePath = @"C:\Users\JSR\Desktop\" + @"\PointLists\" + fileName + DateTime.Now.ToString("yyyyMMddHH") + ".csv";
-            string DirPath = @"C:\Users\JSR\Desktop\" + @"\PointLists";
+            string FilePath = @"C:\BEMS_PointList\" + fileName + DateTime.Now.ToString("yyyyMMddHH") + ".csv";
+            string DirPath = @"C:\BEMS_PointList";
             string temp;
 
             DirectoryInfo di = new DirectoryInfo(DirPath);
@@ -135,6 +171,47 @@ namespace WindowsFormsApplication1
             {
                // Log(string.Format("{1} {0}", e.ToString(), GetDateTime()));
             }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (que.Count > 0)
+            {
+                for (int i = 0; i < que.Count; i++)
+                {
+                    //tbLog.Text += que.Dequeue() + "\r\n"; 
+                    var outputFileName = tbFileUrl.Text.Contains("MODBUS") ? "MODBUS" : "BACNET";
+                    Log(que.Dequeue(), "BEMS_" + outputFileName + "_");
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("CSV 파일 생성 완료");
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (queSql.Count > 0)
+            {
+                for (int i = 0; i < queSql.Count; i++)
+                {
+                    SqlConnection sqlConn = new SqlConnection(tbSql.Text);
+                    SqlCommand sqlComm = new SqlCommand();
+                    sqlComm = sqlConn.CreateCommand();
+                    sqlComm.CommandText = queSql.Dequeue();
+                    sqlConn.Open();
+                    sqlComm.ExecuteNonQuery();
+                    sqlConn.Close();
+
+                }
+            }
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("DB에 넣기 완료");
         }
     }
 }
